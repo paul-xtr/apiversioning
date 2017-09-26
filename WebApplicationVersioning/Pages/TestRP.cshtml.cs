e16c12ec624d298ce001924f62b5cbf29cc5dd48
paul <paul@xtrsolutions.co.uk>
2017-09-26 21:05:01 +0100

    Add project files.

---
+++ WebApplicationVersioning/Pages/TestRP.cshtml.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplicationVersioning.Pages
+{
+    [ApiVersionNeutral]
+    public class TestRPModel : PageModel
+    {
+       
+
+        public void OnGet()
+        {
+        }
+    }
+}
